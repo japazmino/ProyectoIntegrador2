3e7ef407ae07518c4bf05a92bbc180da25be2d21
Pazmino <japazmino5@hotmail.com>
2016-03-04 07:46:31 -0500

    Comentario recibir

--- Pazmino_SemanaSeis/src/Comunicacion/Comunicacion.java
+++ Pazmino_SemanaSeis/src/Comunicacion/Comunicacion.java
@@ -58,6 +58,7 @@ public class Comunicacion extends Thread {
 		}
 	}
 
+	// Recibir
 	public void recibir() {
 		byte[] buzon = new byte[1024];
 		DatagramPacket pack = new DatagramPacket(buzon, buzon.length);
